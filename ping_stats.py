bda04f0d8ed32713921de192a04b68f11998c0bd
= <allison.butler1414@gmail.com>
2017-04-03 18:31:02 -0400

    commented code

--- ping_stats.py
+++ ping_stats.py
@@ -1,18 +1,26 @@
+#note: this script saves the graph instead of showing it because i'm running it headlessly on a raspberry pi so i can't see the graph.
+
+#import all the libraries
 import numpy as np
 import matplotlib
 matplotlib.use('Agg')
 
 import matplotlib.pyplot as plt
 
+#initialize the error file
 wFileName = "errorLog.txt"
 wFile = open(wFileName, 'w')
 
+#read the data from the text file
 with open("ping_data.txt") as f:
     content = f.readlines()
 
+#initialize labels
 cdfVals = []
 errors = []
 step = 0.1
+
+#read in the file and sanitize data
 for line in content:
     if line[0] == '6':
         start = line.index("time=") + 5
@@ -23,21 +31,26 @@ for line in content:
     else:
         errors.append(line)
 
+#set axes labels
 plt.ylabel("Probability")
 plt.xlabel("Ping Time (ms)")
+#convert strings to floats for calculations
 cdfVals = np.array(cdfVals).astype(np.float)
+#sort values
 sortCdf = np.sort(cdfVals)
 x = np.sort(cdfVals)
-print x[0]
+#normalize values for pdf
 sortCdf /= (step*sortCdf).sum()
+#get cumulative values
 cy = np.cumsum(sortCdf*step)
 
-print x[0]
+#create plot
 plt.plot(x,cy,'b--')
-#n, bins, patch = plt.hist(sortCdf,bins=100,facecolor='purple',alpha=0.25,cumulative=True)
 
+#save as a png
 plt.savefig('cdf.png')
 
+#if any errors were found, log them here
 for error in errors:
     wFile.write(error)
     wFile.write("\n")
