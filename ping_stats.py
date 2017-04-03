37a3f1180e603289eadc51501c5405beddf00d65
= <allison.butler1414@gmail.com>
2017-04-03 17:20:38 -0400

    bigger picture

--- ping_stats.py
+++ ping_stats.py
@@ -4,22 +4,28 @@ matplotlib.use('Agg')
 
 import matplotlib.pyplot as plt
 
+wFileName = "cleanData.txt"
+wFile = open(wFileName, 'w')
+
 with open("ping_data.txt") as f:
     content = f.readlines()
 
 cdfVals = []
 errors = []
 
-for line in content[:100]:
+for line in content[:1000]:
     if line[0] == '6':
         start = line.index("time=") + 5
         val = line[start:]
         end = val.index(" ");
         val = val[:end]
         cdfVals.append(val)
+        wFile.write(val)
+        wFile.write("\n")
     else:
         errors.append(line)
 
+wFile.close()
 num_bins = 20
 fig = plt.figure()
 ax = fig.add_subplot(111)
